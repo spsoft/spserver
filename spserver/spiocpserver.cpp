7ef5a9cf62fe480d9cfdd98e514914a8c36fb50a
stephen.nil <stephen.nil@7980853b-782b-0410-b188-7946ed1c5cad>
2008-05-27 15:48:02 +0000

    AcceptEx return BOOL, not int

--- spserver/spiocpserver.cpp
+++ spserver/spiocpserver.cpp
@@ -146,11 +146,11 @@ sp_thread_result_t SP_THREAD_CALL SP_IocpServer :: acceptThread( void * arg )
 		SP_IOUtils::setNonblock( (int)acceptArg->mClientSocket );
 		memset( &( acceptArg->mOverlapped ), 0, sizeof( OVERLAPPED ) );
 
-		int ret = AcceptEx( (SOCKET)acceptArg->mListenSocket, (SOCKET)acceptArg->mClientSocket,
+		BOOL ret = AcceptEx( (SOCKET)acceptArg->mListenSocket, (SOCKET)acceptArg->mClientSocket,
 				acceptArg->mBuffer,	0, sizeof(struct sockaddr_in) + 16, sizeof(struct sockaddr_in) + 16,
 				&recvBytes, &( acceptArg->mOverlapped ) );
 
-		if( ret == SOCKET_ERROR && (ERROR_IO_PENDING != WSAGetLastError()) ) {
+		if( FALSE == ret && (ERROR_IO_PENDING != WSAGetLastError()) ) {
 			sp_syslog( LOG_ERR, "AcceptEx() fail, errno %d", WSAGetLastError() );
 			closesocket( (int)acceptArg->mClientSocket );
 		} else {
